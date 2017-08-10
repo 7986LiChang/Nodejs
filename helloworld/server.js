eac6a52c0180d08e62f1c985a2d1cf827d1418af
7986LiChang <798602874@qq.com>
2017-08-10 23:10:24 +0800

    NodeJS的文件系统接口

--- helloworld/server.js
+++ helloworld/server.js
@@ -1,15 +1,29 @@
 'use strict';
 
-var http = require('http');
+var http = require('http'),
+    fs = require('fs');    //NodeJS提供的文件系统接口
 
-http.createServer(function (request, response){
-	//响应HTTP头部
-	//HTTP状态值：200：OK
-	// 内容类型：text/palin
-	response.writeHead(200,{'Content-Type':'text/html'});
 
-	// 发送响应数据“Hello World!”
-	response.end('<h1>My first Nodejs Server</h1>');
-}).listen(8888);
+/**
+ * [通过文件接口读取本地文件]
+ * @param  {[type]} err       [description]
+ * @param  {[type]} html){} [文件内容]
+ * @return {[type]}           [description]
+ */
+fs.readFile('index.html', function(err, html){
+	if(err){
+		throw err;
+	}
+	http.createServer(function (request, response){
+		//响应HTTP头部
+		//HTTP状态值：200：OK
+		// 内容类型：text/palin
+		response.writeHead(200,{'Content-Type':'text/html'});
+
+		// 发送响应数据“Hello World!”
+		response.end(html);
+	}).listen(8888);
+});
+
 
 console.log('Server running at http://127.0.0.1:8888');
